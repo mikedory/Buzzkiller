a4edc30380cfcfd2b73894a1309d9c6bc2fb1d27
mjd <mjd@doryexmachina.com>
2011-03-06 16:43:19 -0500

    better logging, added parsing, all-around radness

---
+++ buzzkiller.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+buzzkiller.py
+
+Created by Michael Dory on 2011-03-05.
+Copyright (c) 2011 DxM. All rights reserved.
+"""
+
+import argparse
+import sys
+# import shlex # the next step is to do better multi-word phrase checking
+
+# this is where the magic happens
+def main(arguments=None):
+		
+	# create the parser
+	parser = arguments(
+			description='Chomp you up some buzzwords.'
+	)
+
+	# check the command line for incoming words
+	parser.add_argument(
+			'checkWords', metavar='str', type=str, nargs='+',
+			help='words you want to check against from the command line'
+	)
+
+	# allow folks to check a file instead of against the command line
+	parser.add_argument(
+			'-f', metavar='open', type=open, nargs='?',
+			help='words you want to check against from a file'
+	)
+
+	# allow folks to check a file instead of against the command line
+	parser.add_argument(
+			'-v', dest='verbose', action='store_true',
+			help='verbose mode'
+	)
+
+	# make a seperate log file, if that's your sort of thing
+	parser.add_argument(
+			'--log', type=argparse.FileType('w'), default=sys.stdout,
+			help='the file where the sum should be written '
+					 '(default: write the sum to stdout)'
+	)
+	
+	# parse the command line
+	args = parser.parse_args()
+
+	# open the files to compare against
+	weasels = readFile("weasels.txt")
+	buzzwords = readFile("buzzwords.txt")
+	
+	# define the document to search through
+	document = []
+
+	# if there is a file specified, let's search that
+	if (args.f is not None):
+		newFile = fileToList(args.f, True)
+		document.extend(newFile)
+	
+	# if there are additional words on the end, let's search them too
+	if (args.checkWords is not None):
+		document.extend(args.checkWords)
+
+	# if we've actually got something, let's check it!
+	if (document is not None):
+		args.log.write('\nAlrighty, let\'s see what you\'ve got here: \n\n')
+
+		# log(weasels and buzzwords when they are found
+		for line in document:
+				line = line.strip()
+				for token in line.split(" "):
+				
+					# call out the weasels
+					if token in weasels:
+						args.log.write('WEASEL: ' + token + '\n')
+
+					# call out the buzzwords
+					if token in buzzwords:
+						args.log.write('BUZZWORD: ' + token + '\n')
+
+
+	# if someone set the -v verbose flag, tell them about the arguments they used
+	if (args.verbose is not False):
+		args.log.write('\n --------------------------- \n\n')
+		args.log.write(' These were your test words: \n')
+		args.log.write('\n --------------------------- \n\n')
+
+		# log(out the list of words you checked against
+		args.log.write('%s \n' % (document))
+		args.log.write('\n --------------------------- \n\n')
+
+		# if there was a file used, print its name
+		if (args.f is not None):
+			args.log.write('And we looked through a file called: %s\n\n' % args.f.name)
+			args.log.write('\n --------------------------- \n\n')
+
+	# close up shop
+	print 'done!'
+	args.log.write('\nHey, that was fun, right? \n')
+	args.log.close()
+
+
+# handy funciton for opening files and returning lists
+def readFile(fileHandle): 
+
+		print('checking file named %s' % fileHandle)
+
+		fileContents = open(fileHandle)
+		wordList = fileToList(fileContents)
+
+		return wordList
+
+# make the incoming file a list
+# optional: split up the file by spaces
+def fileToList(file,split=False):
+	inputList = []
+
+	# grab the file
+	with file as f:
+
+		# go line-by-line with it
+		for line in f:
+			line = line.strip()
+
+			# if we asked for a split, split it up by spaces
+			if (split == True):
+				for l in line.split(" "):
+					inputList.append(l)
+			else:
+				inputList.append(line)
+
+		# fire it back
+		return inputList
+
+	
+# do that thang!
+if __name__ == "__main__":
+	print ' '
+	print ' ------------ '
+	print ' Here goes... '
+	print ' ------------ '
+	print ' '
+	sys.exit(main(argparse.ArgumentParser))
+
